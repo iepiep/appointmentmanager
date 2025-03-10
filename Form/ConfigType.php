44058240d10bcd41f57d57fb0aeea85efe0c1b40
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-03-10 11:42:52 +0100

    Update ConfigType.php

--- Form/ConfigType.php
+++ Form/ConfigType.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * ConfigType form
+ * @author Roberto Minini <r.minini@solution61.fr>
+ * @copyright 2025 Roberto Minini
+ * @license MIT
  *
- * Author: Roberto Minini (iepiep74@gmail.com)
+ * This file is part of the AppointmentManager Module
  * License: MIT License
  */
 
