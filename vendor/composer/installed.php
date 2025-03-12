7ab7af275d04348ac87c64b306b3936c788daf56
Roberto Minini <iepiep74@gmail.com>
2025-03-12 10:39:25 +0100

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2cd9c18a9fdd3585e79c4fe90229e90b586e6e0e',
+        'name' => 'solution61/appointmentmanager',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'solution61/appointmentmanager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2cd9c18a9fdd3585e79c4fe90229e90b586e6e0e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
