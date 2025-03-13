f9d1302f15a7b2c933524cca121092f9d641c799
Roberto Minini <iepiep74@gmail.com>
2025-03-13 20:23:26 +0100

    a

---
+++ src/Service/TestService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PrestaShop\Module\AppointmentManager\Service;
+
+class TestService
+{
+    public function testMethod(): string
+    {
+        return "Test Service is working!";
+    }
+}
