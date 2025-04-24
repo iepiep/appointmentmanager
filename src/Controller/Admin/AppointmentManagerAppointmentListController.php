84166570414b09cbea4007bbb62433339491440e
Roberto Minini <iepiep74@gmail.com>
2025-04-24 11:05:49 +0200

    a

--- src/Controller/Admin/AppointmentManagerAppointmentListController.php
+++ src/Controller/Admin/AppointmentManagerAppointmentListController.php
@@ -8,21 +8,31 @@
  * License: MIT License
  */
 
- namespace PrestaShop\Module\AppointmentManager\Controller\Admin;
+namespace PrestaShop\Module\AppointmentManager\Controller\Admin;
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+use Db;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AppointmentManagerAppointmentListController extends FrameworkBundleAdminController
 {
-    public function index(Request $request)
+    public function index(Request $request): Response
     {
-        $sql = 'SELECT * FROM `'._DB_PREFIX_.'appointment_manager`';
-        $appointments = \Db::getInstance()->executeS($sql);
-        return $this->render('@Modules/appointmentmanager/views/templates/admin/customer_list.html.twig', ['appointments' => $appointments]);
+
+        $sql = 'SELECT * FROM `' . _DB_PREFIX_ . 'appointment_manager`';
+
+        $appointments = Db::getInstance()->executeS($sql);
+
+        return $this->render(
+            '@Modules/appointmentmanager/views/templates/admin/customer_list.html.twig',
+            [
+                'appointments' => $appointments,
+            ]
+        );
     }
 }
