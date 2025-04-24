585990bd26a518d621b78b1c52115acc8f7cda3e
Roberto Minini <iepiep74@gmail.com>
2025-04-24 11:29:10 +0200

    A

--- src/Form/AppointmentManagerConfigFormType.php
+++ src/Form/AppointmentManagerConfigFormType.php
@@ -1,21 +1,25 @@
 <?php
 /**
-* @author Roberto Minini <r.minini@solution61.fr>
-* @copyright 2025 Roberto Minini
-* @license MIT
-*
-* This file is part of the AppointmentManager project.
-* This source file is subject to the MIT license that is bundled
-* with this source code in the file LICENSE.
-*/
+ * @author Roberto Minini <r.minini@solution61.fr>
+ * @copyright 2025 Roberto Minini
+ * @license MIT
+ *
+ * This file is part of the AppointmentManager project.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 declare(strict_types=1);
 
 namespace PrestaShop\Module\AppointmentManager\Form;
 
 use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -30,27 +34,39 @@ class AppointmentManagerConfigFormType extends TranslatorAwareType
                 'google_api_key',
                 TextType::class,
                 [
-                    'label' => $this->trans('Google API key', 'Modules.AppointmentManager.Admin'),
-                    'required'   => true,
-                    'help' => $this->trans('Maximum 40 characters', 'Modules.AppointmentManager.Admin'),
+                    'label' => $this->trans('Google API key', 'Modules.Appointmentmanager.Admin'),
+                    'required' => true,
+                    'help' => $this->trans('Enter your Google Maps API key.', 'Modules.Appointmentmanager.Admin'),
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length(['max' => 40]),
+                    ],
                 ]
             )
             ->add(
                 'appointment_length',
-                TextType::class,
+                NumberType::class,
                 [
-                    'label' => $this->trans('Appointment length', 'Modules.AppointmentManager.Admin'),
-                    'required'   => true,
-                    'help' => $this->trans('Enter appointment length in minutes (30-240)', 'Modules.AppointmentManager.Admin'),
+                    'label' => $this->trans('Appointment length (minutes)', 'Modules.Appointmentmanager.Admin'),
+                    'required' => true,
+                    'help' => $this->trans('Default duration for each appointment.', 'Modules.Appointmentmanager.Admin'),
+                    'constraints' => [
+                        new NotBlank(),
+                        new Range(['min' => 30, 'max' => 240]),
+                    ],
                 ]
             )
             ->add(
                 'lunch_break_length',
-                TextType::class,
+                NumberType::class,
                 [
-                    'label' => $this->trans('Lunch break length', 'Modules.AppointmentManager.Admin'),
-                    'required'   => true,
-                    'help' => $this->trans('Enter lunch break length in minutes (0-90)', 'Modules.AppointmentManager.Admin'),
+                    'label' => $this->trans('Lunch break length (minutes)', 'Modules.Appointmentmanager.Admin'),
+                    'required' => true,
+                    'help' => $this->trans('Duration of the lunch break (0 for no break).', 'Modules.Appointmentmanager.Admin'),
+                    'constraints' => [
+                        new NotBlank(), // If 0 is allowed, maybe NotNull is better, but NotBlank ensures *something* is entered.
+                        new Range(['min' => 0, 'max' => 90]),
+                    ],
                 ]
             );
     }
